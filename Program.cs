00cc64992b3af3d23493f4b1c728afa3e100372f
Amr Gamal <amr.aboulela97@gmail.com>
2024-01-21 14:41:39 +0200

    added program.cs file that contains the 2 classes

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace GitTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+    class HelloWorld
+    {
+       public static void PrintHelloWorld()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    class HelloITI
+    {
+        public static void PrintHelloITI()
+        {
+            Console.WriteLine("HelloITI");
+        }
+    }
+}
